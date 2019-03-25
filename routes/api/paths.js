9fdee7a4dbf1ecac8b117dab72e874b5dc7c66d1
codeindustry <matt.codeindustry@gmail.com>
2019-03-25 14:33:07 +0000

    deletePath() add delete method on path route

--- routes/api/paths.js
+++ routes/api/paths.js
@@ -12,15 +12,26 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
     const paths = await loadPathsCollection();
     await paths.insertOne({
-        slug: req.body.text,
+        type: req.body.path.type,
+        title: req.body.path.title,
+        description: req.body.path.description,
+        name: req.body.path.name,
+        data: req.body.path.data,
         created_at: new Date()
     });
     res.status(201).send();
 });
 
+router.delete('/:id', async (req, res, next) => {
+    const paths = await loadPathsCollection();
+    console.log('req.params.id', req.params.id);
+    await paths.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
 async function loadPathsCollection() {
     const client = await mongodb.MongoClient.connect(
-        'mongodb+srv://mattanderson:test123@cluster0-qxui0.mongodb.net/buetify?retryWrites=true',
+        'mongodb+srv://' + process.env.DBUSER + ':' + process.env.DBPASS + '@cluster0-qxui0.mongodb.net/' + process.env.DATABASE + '?retryWrites=true',
         {useNewUrlParser: true}
     );
     return client.db('buetify').collection('paths');
